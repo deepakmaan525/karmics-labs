ebf2e54aa28c1359352ec5ac69e941a788f2c80e
deepakmaan525 <deepakmaan525@gmail.com>
2023-09-11 10:31:37 +0530

    modify-navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,5 @@ let menu = document.querySelector(".menu")
 
 menu.addEventListener("click",function(){
     mobileView.classList.toggle("show")
+    document.querySelector("body").classList.toggle("overflow-hidden")
 })
